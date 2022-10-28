f7c93864ad3437647ed5b6e9b7c92b71191dfb54
chusiping <chusiping@163.com>
2022-10-28 11:26:03 +0800

    2022-10-28 11:24  删除lock旧文件, npm install成功预览添加favorite.ico

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,7 +9,7 @@ export function resetTokenAndClearUser() {
     resetRouter()
 }
 
-export const defaultDocumentTitle = 'vue-admin-template'
+export const defaultDocumentTitle = '15home-vue-template'
 export function getDocumentTitle(pageTitle) {
     if (pageTitle) return `${defaultDocumentTitle} - ${pageTitle}`
     return `${defaultDocumentTitle}`
