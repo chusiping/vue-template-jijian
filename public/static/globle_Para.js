bc34f6315db40501127c5f418f9edb4c3225e9f2
chusiping <chusiping@163.com>
2023-05-19 14:57:40 +0800

    2023-5-19 14:56 edit 全局变量api_getStockPrice删除域名改为相对/vhost

--- public/static/globle_Para.js
+++ public/static/globle_Para.js
@@ -1,6 +1,6 @@
 // 如何添加全局变量 https://www.jianshu.com/p/eb8aa239be07
 
 const globle_Para = {
-    api_getStockPrice : "http://typecho.qy:8001/vhost/custom/api_stock_docker.php",
+    api_getStockPrice : "/vhost/custom/api_stock_docker.php",
     api_getZxg : "http://121.4.43.207:3000/myStockExist" 
 }
